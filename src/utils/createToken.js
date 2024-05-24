a90b09cab28be5d0dafdc83da5b15463e92656c4
aren talb <aren.talb00@gmail.com>
2024-05-24 04:43:52 +0300

    Change sameSite to be none

--- src/utils/createToken.js
+++ src/utils/createToken.js
@@ -8,7 +8,7 @@ function createToken(res, userId) {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure in production
-    sameSite: "lax", // Use 'lax' instead of 'none'
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 
