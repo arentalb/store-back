a1ce2bbb19e995c77f70d434dea65e5f7ef93dda
aren talb <aren.talb00@gmail.com>
2024-06-19 05:17:53 +0300

    Change the email sender to send email to gmail instead of mailtrap

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,24 +1,34 @@
-import nodemailer from "nodemailer"
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const emailTemplateGenerator = (message, shortMessage, link) => {
     return `
-            <div style="font-family: Arial, sans-serif; line-height: 1.6;">
-                <p style="font-size: 16px; color: #333;">${message}</p>
-                <p><a href="${link}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #007BFF; text-decoration: none; border-radius: 5px;">Link :)</a></p>
-            </div>
-        `;
-}
+        <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+            <p style="font-size: 16px; color: #333;">${message}</p>
+            <p><a href="${link}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #007BFF; text-decoration: none; border-radius: 5px;">${shortMessage}</a></p>
+        </div>
+    `;
+};
+
 const sendEmail = async (options) => {
     try {
         // 1 create a transporter
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
+            secure: false, // true for 465, false for other ports
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD,
             },
+            tls: {
+                rejectUnauthorized: false
+            }
         });
+
         const htmlContent = emailTemplateGenerator(
             options.message,
             "Verify Email", // shortMessage
@@ -27,7 +37,7 @@ const sendEmail = async (options) => {
 
         // 2 define email options
         const mailOptions = {
-            from: "Aren Talb <aren.talb00@gmail.com>",
+            from: `Aren Talb <${process.env.EMAIL_USERNAME}>`,
             to: options.email,
             subject: options.subject, // Correctly use options.subject
             text: options.message, // Plain text body
