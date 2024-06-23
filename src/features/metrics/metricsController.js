21582b9640fdce65089a76c8da3959eeb70cadb8
aren talb <aren.talb00@gmail.com>
2024-06-23 04:58:56 +0300

    Return more metrics to show them by chart in frontend

--- src/features/metrics/metricsController.js
+++ src/features/metrics/metricsController.js
@@ -1,16 +1,17 @@
-import {sendSuccess} from "../../utils/resposeSender.js";
-import catchAsync from "../../utils/catchAsync.js";
 import Order from "../order/Order.js";
 import User from "../user/User.js";
+import {sendSuccess} from "../../utils/resposeSender.js";
+import catchAsync from "../../utils/catchAsync.js";
 
-// Get metrics data
 const getMetrics = catchAsync(async (req, res) => {
     const metrics = await getMetricsData();
     sendSuccess(res, metrics, 200);
 });
 
 const getMetricsData = async () => {
-    const [totalSales, totalOrders, avgOrderValue, topSellingProducts, newUsers] = await Promise.all([
+    const startOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+
+    const [totalSales, totalOrders, avgOrderValue, topSellingProducts, newUsers, salesData, ordersData, orderValueData, newUsersData] = await Promise.all([
         Order.aggregate([
             {$match: {isPaid: true}},
             {$group: {_id: null, totalSales: {$sum: "$totalPrice"}}},
@@ -35,9 +36,53 @@ const getMetricsData = async () => {
             {$project: {_id: 0, productId: "$_id", name: "$product.name", totalSold: 1}}
         ]),
         User.aggregate([
-            {$match: {createdAt: {$gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1)}}},
+            {$match: {createdAt: {$gte: startOfMonth}}},
             {$count: "newUsers"}
-        ])
+        ]),
+        Order.aggregate([
+            {$match: {isPaid: true}},
+            {
+                $group: {
+                    _id: {$dateToString: {format: "%Y-%m-%d", date: "$createdAt"}},
+                    totalSales: {$sum: "$totalPrice"}
+                }
+            },
+            {$sort: {_id: 1}},
+            {$project: {date: "$_id", totalSales: 1, _id: 0}}
+        ]),
+        Order.aggregate([
+            {$match: {isPaid: true}},
+            {
+                $group: {
+                    _id: {$dateToString: {format: "%Y-%m-%d", date: "$createdAt"}},
+                    totalOrders: {$count: {}}
+                }
+            },
+            {$sort: {_id: 1}},
+            {$project: {date: "$_id", totalOrders: 1, _id: 0}}
+        ]),
+        Order.aggregate([
+            {$match: {isPaid: true}},
+            {
+                $group: {
+                    _id: {$dateToString: {format: "%Y-%m-%d", date: "$createdAt"}},
+                    avgOrderValue: {$avg: "$totalPrice"}
+                }
+            },
+            {$sort: {_id: 1}},
+            {$project: {date: "$_id", avgOrderValue: 1, _id: 0}}
+        ]),
+        User.aggregate([
+            {$match: {createdAt: {$gte: startOfMonth}}},
+            {
+                $group: {
+                    _id: {$dateToString: {format: "%Y-%m-%d", date: "$createdAt"}},
+                    newUsers: {$count: {}}
+                }
+            },
+            {$sort: {_id: 1}},
+            {$project: {date: "$_id", newUsers: 1, _id: 0}}
+        ]),
     ]);
 
     return {
@@ -45,7 +90,11 @@ const getMetricsData = async () => {
         totalOrders: totalOrders[0]?.totalOrders || 0,
         avgOrderValue: avgOrderValue[0]?.avgOrderValue || 0,
         topSellingProducts: topSellingProducts || [],
-        newUsers: newUsers[0]?.newUsers || 0
+        newUsers: newUsers[0]?.newUsers || 0,
+        salesData: salesData || [],
+        ordersData: ordersData || [],
+        orderValueData: orderValueData || [],
+        newUsersData: newUsersData || []
     };
 };
 
